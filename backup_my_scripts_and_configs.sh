68d419f51718ecbf518d47290c44b4cb9405806a
ZyMOS <user@server.fake>
2013-12-20 23:32:31 -0700

    scripted full commit

--- backup_my_scripts_and_configs.sh
+++ backup_my_scripts_and_configs.sh
@@ -2,4 +2,9 @@
 
 cp /usr/scripts/* /mnt/tb2/online.storage/github/scripts
 
+cp ~/scriptsPersonal/*.* /mnt/tb2/online.storage/github/scripts_personal
+
+
+cp /etc/vim/vimrc.local global_etc_files 
+cp /etc/profile.d/*.* global_etc_files
 
